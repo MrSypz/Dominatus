ec7fd6e5d19d5553389e98a3ba404f5820216af7
chai <chainot@hotmail.co.th>
2025-02-10 16:22:27 +0700

    Config Implement

--- src/main/java/sypztep/dominatus/ModConfig.java
+++ src/main/java/sypztep/dominatus/ModConfig.java
@@ -3,7 +3,16 @@ package sypztep.dominatus;
 
 import me.shedaniel.autoconfig.ConfigData;
 import me.shedaniel.autoconfig.annotation.Config;
+import me.shedaniel.autoconfig.annotation.ConfigEntry;
+import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
 
 @Config(name = Dominatus.MODID)
 public class ModConfig implements ConfigData {
+    @ConfigEntry.Category("feature-client")
+    @Comment("Damage Indicator Feature (default : true) if you want to turn it off I think delete mod will be more better :)")
+    public static boolean damageCritIndicator = true;
+    @ConfigEntry.Category("feature-client")
+    @ConfigEntry.ColorPicker()
+    @Comment("Color of the crit indicator")
+    public static int critDamageColor = 0xFF4F00;
 }
--- src/main/java/sypztep/dominatus/client/DominatusClient.java
+++ src/main/java/sypztep/dominatus/client/DominatusClient.java
@@ -1,19 +1,26 @@
 package sypztep.dominatus.client;
 
+import me.shedaniel.autoconfig.AutoConfig;
+import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
+import sypztep.dominatus.ModConfig;
 import sypztep.dominatus.client.screen.PlayerInfoScreen;
 
 public class DominatusClient implements ClientModInitializer {
     public static KeyBinding stats_screen = new KeyBinding("key.dominatus.debug", InputUtil.Type.KEYSYM, GLFW.GLFW_KEY_I, "category.dominatus.keybind");
+    public static ModConfig config = new ModConfig();
 
     @Override
     public void onInitializeClient() {
         ClientTickEvents.END_CLIENT_TICK.register(DominatusClient::onEndTick);
+
+        AutoConfig.register(ModConfig.class, GsonConfigSerializer::new);
+        config = AutoConfig.getConfigHolder(ModConfig.class).getConfig();
 
     }
     private static void onEndTick(MinecraftClient client) {
