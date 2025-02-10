b3aa04573fd44e3042c022a2010b701d6ad4acd9
chai <chainot@hotmail.co.th>
2025-02-11 03:39:48 +0700

    remove mixin

--- src/main/java/sypztep/dominatus/Dominatus.java
+++ src/main/java/sypztep/dominatus/Dominatus.java
@@ -2,9 +2,12 @@ package sypztep.dominatus;
 
 import net.fabricmc.api.ModInitializer;
 import net.minecraft.util.Identifier;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 public class Dominatus implements ModInitializer {
     public static final String MODID = "dominatus";
+    public static final Logger LOGGER = LogManager.getLogger(MODID);
     public static Identifier id(String path) {
         return Identifier.of(MODID, path);
     }
