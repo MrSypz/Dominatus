10a49111271a5117d7df1de3b825043538436c9f
chai <chainot@hotmail.co.th>
2025-02-10 10:24:08 +0700

    increase range

--- src/main/java/sypztep/dominatus/common/init/ModEntityAttributes.java
+++ src/main/java/sypztep/dominatus/common/init/ModEntityAttributes.java
@@ -15,14 +15,13 @@ public class ModEntityAttributes {
     public static final RegistryEntry<EntityAttribute> GENERIC_CRIT_DAMAGE;
     public static final RegistryEntry<EntityAttribute> GENERIC_CRIT_CHANCE;
 
-
     static {
-        GENERIC_AP = register("generic.ap", new ClampedEntityAttribute("attribute.name.generic.ap", 0, 0.0, 1000.0));
-        GENERIC_DP = register("generic.dp", new ClampedEntityAttribute("attribute.name.generic.dp", 0, 0.0, 1000.0));
-        GENERIC_ACCURACY = register("generic.accuracy", new ClampedEntityAttribute("attribute.name.generic.accuracy", 50, 0.0, 500.0).setTracked(true));
-        GENERIC_EVASION = register("generic.evasion", new ClampedEntityAttribute("attribute.name.generic.evasion", 20, 0.0, 500.0).setTracked(true));
-        GENERIC_CRIT_CHANCE = register("generic.crit_chance", new ClampedEntityAttribute("attribute.name.generic.crit_chance", 0, 0.0, 1.0).setTracked(true));
-        GENERIC_CRIT_DAMAGE = register("generic.crit_damage", new ClampedEntityAttribute("attribute.name.generic.crit_damage", 0.5, 0.0, 64.0).setTracked(true));
+        GENERIC_AP = register("generic.ap", new ClampedEntityAttribute("attribute.name.generic.ap", 0, 0.0, 1024.0D));
+        GENERIC_DP = register("generic.dp", new ClampedEntityAttribute("attribute.name.generic.dp", 0, 0.0, 1024.0D));
+        GENERIC_ACCURACY = register("generic.accuracy", new ClampedEntityAttribute("attribute.name.generic.accuracy", 50, 0.0, 2048.0D).setTracked(true));
+        GENERIC_EVASION = register("generic.evasion", new ClampedEntityAttribute("attribute.name.generic.evasion", 20, 0.0, 2048.0D).setTracked(true));
+        GENERIC_CRIT_CHANCE = register("generic.crit_chance", new ClampedEntityAttribute("attribute.name.generic.crit_chance", 5, 0.0, 200.0D).setTracked(true));
+        GENERIC_CRIT_DAMAGE = register("generic.crit_damage", new ClampedEntityAttribute("attribute.name.generic.crit_damage", 50, -100, 1024.0D).setTracked(true));
     }
 
     private static RegistryEntry<EntityAttribute> register(String id, EntityAttribute attribute) {
