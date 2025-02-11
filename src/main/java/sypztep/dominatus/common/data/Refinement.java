5a1322853697811ede76cd955cee2df9cbaed8a5
chai <chainot@hotmail.co.th>
2025-02-11 20:02:46 +0700

    Component type

---
+++ src/main/java/sypztep/dominatus/common/data/Refinement.java
@@ -0,0 +1,15 @@
+package sypztep.dominatus.common.data;
+
+import com.mojang.serialization.Codec;
+import com.mojang.serialization.codecs.RecordCodecBuilder;
+
+public record Refinement(int refine, int accuracy, int evasion, int durability, float damage, float protection) {
+    public static final Codec<Refinement> CODEC = RecordCodecBuilder.create(instance -> instance.group(
+            Codec.INT.fieldOf("refine").forGetter(Refinement::refine),
+            Codec.INT.fieldOf("accuracy").forGetter(Refinement::accuracy),
+            Codec.INT.fieldOf("evasion").forGetter(Refinement::evasion),
+            Codec.INT.optionalFieldOf("durability",100).forGetter(Refinement::durability),
+            Codec.FLOAT.optionalFieldOf("damage",0.0f).forGetter(Refinement::damage),
+            Codec.FLOAT.optionalFieldOf("protection",0.0f).forGetter(Refinement::protection)
+    ).apply(instance, Refinement::new));
+}
---
+++ src/main/java/sypztep/dominatus/common/init/ModDataComponents.java
@@ -0,0 +1,15 @@
+package sypztep.dominatus.common.init;
+
+import net.minecraft.component.ComponentType;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
+import sypztep.dominatus.Dominatus;
+import sypztep.dominatus.common.data.Refinement;
+
+public class ModDataComponents {
+    public static final ComponentType<Refinement> PENOMIOR = new ComponentType.Builder<Refinement>().codec(Refinement.CODEC).build();
+
+    public static void init() {
+        Registry.register(Registries.DATA_COMPONENT_TYPE, Dominatus.id("refinement"), PENOMIOR);
+    }
+}
