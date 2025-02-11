39db31377418ef4d956b970f350cef73143d7aa2
chai <chainot@hotmail.co.th>
2025-02-11 19:58:37 +0700

    move Knumber to native

---
+++ src/main/java/sypztep/dominatus/client/init/ModParticle.java
@@ -0,0 +1,16 @@
+package sypztep.dominatus.client.init;
+
+import net.minecraft.text.Text;
+import sypztep.dominatus.ModConfig;
+import sypztep.dominatus.client.util.TextParticleProvider;
+
+import java.awt.*;
+
+public class ModParticle {
+    public static TextParticleProvider CRITICAL;
+    public static TextParticleProvider MISSING;
+    static {
+        CRITICAL = TextParticleProvider.register(Text.translatable("dominatus.text.critical"), new Color(ModConfig.critDamageColor), -0.055f, -0.045F, () -> ModConfig.damageCritIndicator);
+        MISSING = TextParticleProvider.register(Text.translatable("dominatus.text.missing"), new Color(255, 255, 255), -0.045f, -1);
+    }
+}
---
+++ src/main/java/sypztep/dominatus/client/payload/AddTextParticlesPayload.java
@@ -0,0 +1,42 @@
+package sypztep.dominatus.client.payload;
+
+import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
+import net.minecraft.entity.Entity;
+import net.minecraft.network.PacketByteBuf;
+import net.minecraft.network.codec.PacketCodec;
+import net.minecraft.network.codec.PacketCodecs;
+import net.minecraft.network.packet.CustomPayload;
+import net.minecraft.server.network.ServerPlayerEntity;
+import sypztep.dominatus.Dominatus;
+import sypztep.dominatus.client.util.TextParticleProvider;
+
+public record AddTextParticlesPayload(int entityId,int selector) implements CustomPayload {
+    public static final Id<AddTextParticlesPayload> ID = new Id<>(Dominatus.id("add_text_particle"));
+    public static final PacketCodec<PacketByteBuf, AddTextParticlesPayload> CODEC = PacketCodec.tuple(
+            PacketCodecs.VAR_INT,
+            AddTextParticlesPayload::entityId,
+            PacketCodecs.UNSIGNED_SHORT, // size 0-65,535
+            AddTextParticlesPayload::selector,
+            AddTextParticlesPayload::new
+    );
+
+    public static void send(ServerPlayerEntity player, int entityId, TextParticleProvider selector) {
+        ServerPlayNetworking.send(player, new AddTextParticlesPayload(entityId, selector.getFlag()));
+    }
+
+    @Override
+    public Id<? extends CustomPayload> getId() {
+        return ID;
+    }
+    public static class Receiver implements ClientPlayNetworking.PlayPayloadHandler<AddTextParticlesPayload> {
+        @Override
+        public void receive(AddTextParticlesPayload payload, ClientPlayNetworking.Context context) {
+            Entity entity = context.player().getWorld().getEntityById(payload.entityId());
+            if (entity != null) {
+                TextParticleProvider.handleParticle(entity, payload.selector());
+            }
+        }
+    }
+}
+
