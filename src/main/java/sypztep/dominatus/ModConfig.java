8b56f0722163fc7a2ec826d94c950fec32684859
chai <chainot@hotmail.co.th>
2025-02-11 20:05:37 +0700

    Config

--- src/main/java/sypztep/dominatus/ModConfig.java
+++ src/main/java/sypztep/dominatus/ModConfig.java
@@ -9,8 +9,11 @@ import me.shedaniel.cloth.clothconfig.shadowed.blue.endless.jankson.Comment;
 @Config(name = Dominatus.MODID)
 public class ModConfig implements ConfigData {
     @ConfigEntry.Category("feature-client")
-    @Comment("Damage Indicator Feature (default : true) if you want to turn it off I think delete mod will be more better :)")
+    @Comment("Crit Indicator (default : true)")
     public static boolean damageCritIndicator = true;
+    @ConfigEntry.Category("feature-client")
+    @Comment("Missing Indicato (default : true)")
+    public static boolean missingIndicator;
     @ConfigEntry.Category("feature-client")
     @ConfigEntry.ColorPicker()
     @Comment("Color of the crit indicator")
--- src/main/java/sypztep/dominatus/client/init/ModParticle.java
+++ src/main/java/sypztep/dominatus/client/init/ModParticle.java
@@ -11,6 +11,6 @@ public class ModParticle {
     public static TextParticleProvider MISSING;
     static {
         CRITICAL = TextParticleProvider.register(Text.translatable("dominatus.text.critical"), new Color(ModConfig.critDamageColor), -0.055f, -0.045F, () -> ModConfig.damageCritIndicator);
-        MISSING = TextParticleProvider.register(Text.translatable("dominatus.text.missing"), new Color(255, 255, 255), -0.045f, -1);
+        MISSING = TextParticleProvider.register(Text.translatable("dominatus.text.missing"), new Color(255, 255, 255), -0.045f, -1, () -> ModConfig.missingIndicator);
     }
 }
