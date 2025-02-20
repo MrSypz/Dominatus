1a790b313b0fcc149711591f671d21e529c8c432
archai <chainot@hotmail.co.th>
2025-02-20 13:43:46 +0700

    Update Refinement

--- src/main/java/sypztep/dominatus/common/util/RefinementUtil.java
+++ src/main/java/sypztep/dominatus/common/util/RefinementUtil.java
@@ -1,9 +1,12 @@
 package sypztep.dominatus.common.util;
 
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.item.ItemStack;
 import sypztep.dominatus.common.data.DominatusItemEntry;
 import sypztep.dominatus.common.data.Refinement;
 import sypztep.dominatus.common.init.ModDataComponents;
+
+import java.util.Random;
 
 public class RefinementUtil {
     public static void setRefinement(ItemStack stack, Refinement refinement) {
@@ -46,16 +49,55 @@ public class RefinementUtil {
 
         setRefinement(stack, refineLvl, newAccuracy, newEvasion, oldRef.durability(), newDamage, newProtection);
     }
-    public static void updateRefinement(ItemStack stack, DominatusItemEntry entry ,int refineLvl) {
-        Refinement oldRef = getRefinement(stack);
+    public static void updateRefinement(ItemStack stack, DominatusItemEntry entry, int refineLvl) {
+        Refinement oldRef = getRefinement(stack);  // Get current refinement
         int newAccuracy = refineValue(refineLvl, entry.maxLvl(), entry.startAccuracy(), entry.endAccuracy());
         int newEvasion = refineValue(refineLvl, entry.maxLvl(), entry.startEvasion(), entry.endAccuracy());
         float newDamage = refineValue(refineLvl, entry.maxLvl(), entry.starDamage(), entry.endDamage());
         int newProtection = refineValue(refineLvl, entry.maxLvl(), entry.startProtection(), entry.endProtection());
 
+        // Debug output to confirm refineLvl change
+        System.out.println("Updating refinement - New refineLvl: " + refineLvl);
+
         setRefinement(stack, refineLvl, newAccuracy, newEvasion, oldRef.durability(), newDamage, newProtection);
     }
 
+
+    public static boolean handleRefine(ItemStack slotOutput, int failStack) {
+        double successRate = FailStackUtil.calculateSuccessRate(slotOutput, failStack);
+        Random random = new Random();
+        double randomValue = random.nextDouble();
+        System.out.println("Success Rate: " + successRate + ", Random Value: " + randomValue);  // Debugging line
+        return randomValue < successRate;
+    }
+
+    public static void processRefinement(ItemStack slotOutput, int failStack, int currentRefineLvl,DominatusItemEntry entry,  PlayerEntity player) {
+        if (handleRefine(slotOutput, failStack)) {
+            handleSuccess(slotOutput, currentRefineLvl, entry, player);
+        } else {
+            handleFailure(slotOutput, failStack, currentRefineLvl, entry, player);
+        }
+    }
+    public static void handleSuccess(ItemStack slotOutput, int currentRefineLvl, DominatusItemEntry entry, PlayerEntity player) {
+        int newRefineLvl = currentRefineLvl + 1;
+        System.out.println(newRefineLvl);
+        updateRefinement(slotOutput,entry,newRefineLvl);
+        FailStackUtil.successRefine(player);
+//        AddRefineSoundPayloadS2C.send(serverPlayer, player.getId(), RefineUtil.RefineSound.SUCCESS.select());
+    }
+
+    public static void handleFailure(ItemStack slotOutput, int failStack, int currentRefineLvl,  DominatusItemEntry entry, PlayerEntity player) {
+        if (currentRefineLvl > 16) { // 17 - 20
+            int newRefineLvl = Math.max(currentRefineLvl - 1, 0);
+            updateRefinement(slotOutput, entry,newRefineLvl);
+//            RefineUtil.setRefineLvl(slotOutput, newRefineLvl);
+//            updateStats(slotOutput, newRefineLvl, maxLvl, startAccuracy, endAccuracy, startEvasion, endEvasion, startDamage, endDamage, startProtect, endProtect);
+        }
+//        int newDurability = Math.max(RefineUtil.getDurability(slotOutput) - 10, 0);
+//        RefineUtil.setDurability(slotOutput, newDurability);
+        FailStackUtil.failRefine(player, failStack);
+//        AddRefineSoundPayloadS2C.send(serverPlayer, player.getId(), RefineUtil.RefineSound.FAIL.select());
+    }
     public static int refineValue(int currentLvl, int maxLvl, int startValue, int endValue) {
         if (currentLvl < 0 || currentLvl > maxLvl)
             throw new IllegalArgumentException("Input value out of range");
