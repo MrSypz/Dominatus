033067b9e61394dd727bfba72f2bed3a8e8449e2
chai <chainot@hotmail.co.th>
2025-02-21 13:38:32 +0700

    Particle

--- src/main/java/sypztep/dominatus/client/init/ModParticle.java
+++ src/main/java/sypztep/dominatus/client/init/ModParticle.java
@@ -9,8 +9,10 @@ import java.awt.*;
 public class ModParticle {
     public static TextParticleProvider CRITICAL;
     public static TextParticleProvider MISSING;
+    public static TextParticleProvider BACKATTACK;
     static {
         CRITICAL = TextParticleProvider.register(Text.translatable("dominatus.text.critical"), new Color(ModConfig.critDamageColor), -0.055f, -0.045F, () -> ModConfig.damageCritIndicator);
         MISSING = TextParticleProvider.register(Text.translatable("dominatus.text.missing"), new Color(255, 255, 255), -0.045f, -1, () -> ModConfig.missingIndicator);
+        BACKATTACK = TextParticleProvider.register(Text.translatable("dominatus.text.back"),new Color(1f,1f,1f),-0.045f,0.15f);
     }
 }
