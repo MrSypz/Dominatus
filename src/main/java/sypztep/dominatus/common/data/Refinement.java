44320c66aeb86a3d002f148d09326624827e47d0
chai <chainot@hotmail.co.th>
2025-03-01 15:32:20 +0700

    Add Comment for my future

--- src/main/java/sypztep/dominatus/common/data/Refinement.java
+++ src/main/java/sypztep/dominatus/common/data/Refinement.java
@@ -3,6 +3,15 @@ package sypztep.dominatus.common.data;
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
 
+/**
+ * ระบบ Refinement แบบ bdo.
+ * @param refine
+ * @param accuracy
+ * @param evasion
+ * @param durability
+ * @param damage
+ * @param protection
+ */
 public record Refinement(int refine, int accuracy, int evasion, int durability, float damage, int protection) {
     public static final Codec<Refinement> CODEC = RecordCodecBuilder.create(instance -> instance.group(
             Codec.INT.fieldOf("refine").forGetter(Refinement::refine),
