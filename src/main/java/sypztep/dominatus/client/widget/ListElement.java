30ddfec4871ff1156c6f72152328f37a7a6eeace
chain <chainot@hotmail.co.th>
2025-03-25 23:08:02 +0700

    Screen element port from penomior on going

---
+++ src/main/java/sypztep/dominatus/client/widget/ListElement.java
@@ -0,0 +1,10 @@
+package sypztep.dominatus.client.widget;
+
+import net.minecraft.text.Text;
+import net.minecraft.util.Identifier;
+
+public record ListElement(Text text, Identifier icon) {
+    public ListElement(Text text) {
+        this(text,null);
+    }
+}
