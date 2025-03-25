112a35406405d8f22e9cab2b4178af671bf0fe64
chain <chainot@hotmail.co.th>
2025-03-25 23:08:28 +0700

    Remove DamageType

--- src/main/java/sypztep/dominatus/client/data/ModDataGenerator.java
+++ src/main/java/sypztep/dominatus/client/data/ModDataGenerator.java
@@ -2,7 +2,6 @@ package sypztep.dominatus.client.data;
 
 import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
 import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
-import sypztep.dominatus.client.data.provider.ModDamageTypeTagProvider;
 import sypztep.dominatus.client.data.provider.ModItemTagProvider;
 import sypztep.dominatus.client.data.provider.ModLanguageProvider;
 import sypztep.dominatus.client.data.provider.RefinementDataProvider;
@@ -11,7 +10,6 @@ public class ModDataGenerator implements DataGeneratorEntrypoint {
     @Override
     public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
         FabricDataGenerator.Pack pack = fabricDataGenerator.createPack();
-        pack.addProvider(ModDamageTypeTagProvider::new);
         pack.addProvider(ModLanguageProvider::new);
         pack.addProvider(RefinementDataProvider::new);
         pack.addProvider(ModItemTagProvider::new);
