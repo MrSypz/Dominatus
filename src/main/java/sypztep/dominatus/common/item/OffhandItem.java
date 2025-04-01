d6c7565fda86898c69f73528cbfb26c72ce2cc91
chain <chainot@hotmail.co.th>
2025-04-01 14:03:10 +0700

    Remove Unused Class

--- src/main/java/sypztep/dominatus/common/item/OffhandItem.java
+++
@@ -1,9 +0,0 @@
-package sypztep.dominatus.common.item;
-
-import io.wispforest.accessories.api.AccessoryItem;
-
-public class OffhandItem extends AccessoryItem {
-    public OffhandItem(Settings properties) {
-        super(properties);
-    }
-}
