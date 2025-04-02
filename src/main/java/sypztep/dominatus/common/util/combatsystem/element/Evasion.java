5c4451e3707e0ef3822d449aa7870ace11813c70
chain <chainot@hotmail.co.th>
2025-04-02 12:42:25 +0700

    simple formula

--- src/main/java/sypztep/dominatus/common/util/combatsystem/element/Evasion.java
+++ src/main/java/sypztep/dominatus/common/util/combatsystem/element/Evasion.java
@@ -3,8 +3,8 @@ package sypztep.dominatus.common.util.combatsystem.element;
 import sypztep.dominatus.common.util.combatsystem.CombatAttribute;
 
 public class Evasion extends CombatAttribute {
-    private static final double BASE_EVASION_CHANCE = 0.08D; // (8% base evasion)
-    private static final double EVASION_SCALING_FACTOR = 0.0015D; // (0.15% per point)
+    private static final double BASE_EVASION = 0.0;
+    private static final double EVASION_SCALING = 0.005;
 
     public Evasion(double baseValue) {
         super(baseValue);
@@ -12,6 +12,6 @@ public class Evasion extends CombatAttribute {
 
     @Override
     public double calculateEffect() {
-        return BASE_EVASION_CHANCE + (getTotalValue() * EVASION_SCALING_FACTOR);
+        return BASE_EVASION + (getTotalValue() * EVASION_SCALING);
     }
 }
