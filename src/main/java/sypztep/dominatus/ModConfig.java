751f53d3a16b9059429df8abde7cc9acf540d70f
chain <chainot@hotmail.co.th>
2025-04-04 00:43:11 +0700

    Attack delay config

--- src/main/java/sypztep/dominatus/ModConfig.java
+++ src/main/java/sypztep/dominatus/ModConfig.java
@@ -18,4 +18,9 @@ public class ModConfig implements ConfigData {
     @ConfigEntry.ColorPicker()
     @Comment("Color of the crit indicator")
     public static int critDamageColor = 0xFF4F00;
+
+    @ConfigEntry.Category("combat")
+    @Comment("the delay damage to appile after player attack")
+    public static float hitdelay = 0.25f;
+
 }
