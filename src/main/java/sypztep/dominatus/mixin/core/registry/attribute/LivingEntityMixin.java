91ea2e05453cf57cdb8a01b593812e1cf53c07ca
chain <chainot@hotmail.co.th>
2025-04-12 05:18:24 +0700

    #1 Fix

--- src/main/java/sypztep/dominatus/mixin/core/registry/attribute/LivingEntityMixin.java
+++ src/main/java/sypztep/dominatus/mixin/core/registry/attribute/LivingEntityMixin.java
@@ -2,6 +2,8 @@ package sypztep.dominatus.mixin.core.registry.attribute;
 
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.attribute.DefaultAttributeContainer;
+import net.minecraft.entity.attribute.EntityAttribute;
+import net.minecraft.registry.entry.RegistryEntry;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
@@ -10,20 +12,10 @@ import sypztep.dominatus.common.init.ModEntityAttributes;
 
 @Mixin(LivingEntity.class)
 public class LivingEntityMixin {
-    @Inject(method = "createLivingAttributes", at = @At("RETURN"), cancellable = true)
+    @Inject(method = "createLivingAttributes", at = @At("RETURN"))
     private static void registryExtraStats(CallbackInfoReturnable<DefaultAttributeContainer.Builder> cir) {
-        DefaultAttributeContainer.Builder builder = cir.getReturnValue();
-        builder
-                .add(ModEntityAttributes.ACCURACY)
-                .add(ModEntityAttributes.EVASION)
-                .add(ModEntityAttributes.CRIT_CHANCE)
-                .add(ModEntityAttributes.CRIT_DAMAGE)
-                .add(ModEntityAttributes.BACK_ATTACK)
-                .add(ModEntityAttributes.AIR_ATTACK)
-                .add(ModEntityAttributes.DOWN_ATTACK)
-                .add(ModEntityAttributes.HEALTH_REGEN)
-                .add(ModEntityAttributes.PLAYER_VERS_ENTITY_DAMAGE)
-                .add(ModEntityAttributes.PLAYER_VERS_PLAYER_DAMAGE);
-        cir.setReturnValue(builder);
+        for (RegistryEntry<EntityAttribute> entry : ModEntityAttributes.ENTRIES) {
+            cir.getReturnValue().add(entry);
+        }
     }
 }
