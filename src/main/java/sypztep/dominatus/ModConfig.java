74d713cd8816d3cafe9a17167423678ed0ba23d5
chain <chainot@hotmail.co.th>
2025-04-22 13:05:21 +0700

    Remove AirborneCombat

--- src/main/java/sypztep/dominatus/ModConfig.java
+++ src/main/java/sypztep/dominatus/ModConfig.java
@@ -25,6 +25,4 @@ public class ModConfig implements ConfigData {
     @ConfigEntry.Category("combat")
     @Comment("the delay damage to appile after player attack")
     public static float hitDelay = 0.25f;
-    @ConfigEntry.Category("combat")
-    public static boolean airborneCombat = true;
 }
