c70e4ad64776efad4b992695a80b984cda8500c0
chain <chainot@hotmail.co.th>
2025-07-19 07:30:58 +0700

    More Damage Attributes

--- src/main/java/sypztep/dominatus/common/init/ModEntityAttributes.java
+++ src/main/java/sypztep/dominatus/common/init/ModEntityAttributes.java
@@ -24,7 +24,12 @@ public final class ModEntityAttributes {
     public static final RegistryEntry<EntityAttribute> CRIT_CHANCE = register("crit_chance", new ClampedEntityAttribute("attribute.name.crit_chance", 0.05, 0.0, 2.0D).setTracked(true));
     public static final RegistryEntry<EntityAttribute> BACK_ATTACK = register("back_attack", new ClampedEntityAttribute("attribute.name.back_attack", 0.5, 0.0, 10.24D).setTracked(true));
 
-    public static final RegistryEntry<EntityAttribute> MAGIC_ATTACK_DAMAGE = register("magic_attack_damage", new ClampedEntityAttribute("attribute.name.magic_attack_damage", 0, 0.0, 4.0).setTracked(true));
+    public static final RegistryEntry<EntityAttribute> MELEE_ATTACK_DAMAGE = register("melee_attack_damage", new ClampedEntityAttribute("attribute.name.melee_attack_damage", 0, 0.0, 4.0).setTracked(true));
+    public static final RegistryEntry<EntityAttribute> MAGIC_ATTACK_DAMAGE = register("magic_attack_damage", new ClampedEntityAttribute("attribute.name.magic_attack_damage", 0, 0.0, 10.24D).setTracked(true));
+    public static final RegistryEntry<EntityAttribute> PROJECTILE_ATTACK_DAMAGE = register("projectile_attack_damage", new ClampedEntityAttribute("attribute.name.projectile_attack_damage", 0, 0.0, 10.24D).setTracked(true));
+
+    public static final RegistryEntry<EntityAttribute> MAGIC_RESISTANCE = register("magic_resistance", new ClampedEntityAttribute("attribute.name.magic_resistance", 0, -10.0D, 0.75D).setTracked(true));
+    public static final RegistryEntry<EntityAttribute> PHYSICAL_RESISTANCE = register("physical_resistance", new ClampedEntityAttribute("attribute.name.physical_resistance", 0, -10.0D, 0.75D).setTracked(true));
 
     private static RegistryEntry<EntityAttribute> register(String id, EntityAttribute attribute) {
         RegistryEntry<EntityAttribute> entry = Registry.registerReference(Registries.ATTRIBUTE, Dominatus.id(id), attribute);
