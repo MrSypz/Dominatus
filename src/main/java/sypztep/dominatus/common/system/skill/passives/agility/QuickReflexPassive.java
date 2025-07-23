7fb2a8d47780c7662699c59800904be8e35f09d2
chain <chainot@hotmail.co.th>
2025-07-23 07:14:17 +0700

    tweak value

--- src/main/java/sypztep/dominatus/common/system/skill/passives/agility/QuickReflexPassive.java
+++ src/main/java/sypztep/dominatus/common/system/skill/passives/agility/QuickReflexPassive.java
@@ -9,7 +9,7 @@ import sypztep.dominatus.common.init.ModEntityAttributes;
 import sypztep.dominatus.common.system.skill.passives.BaseAttributePassive;
 
 /**
- * AGI 20 - Quick Reflex: +5 Evasion
+ * AGI 20 - Quick Reflex: +15 Evasion
  */
 public class QuickReflexPassive extends BaseAttributePassive {
     private static final Identifier MODIFIER_ID = Dominatus.id("passive_quick_reflex");
@@ -18,7 +18,7 @@ public class QuickReflexPassive extends BaseAttributePassive {
         super(
                 Dominatus.id("quick_reflex"),
                 Text.literal("§aQuick Reflex"),
-                Text.literal("Your quick reflexes increase evasion by 5"),
+                Text.literal("Your quick reflexes increase evasion by 15"),
                 "agility",
                 20,
                 2
@@ -27,7 +27,7 @@ public class QuickReflexPassive extends BaseAttributePassive {
 
     @Override
     protected void applyAttributeModifiers(LivingEntity entity) {
-        applyAttributeModifier(entity, ModEntityAttributes.EVASION, MODIFIER_ID, 5.0, EntityAttributeModifier.Operation.ADD_VALUE);
+        applyAttributeModifier(entity, ModEntityAttributes.EVASION, MODIFIER_ID, 15.0, EntityAttributeModifier.Operation.ADD_VALUE);
     }
 
     @Override
--- src/main/java/sypztep/dominatus/common/system/skill/passives/agility/ShadowStepPassive.java
+++ src/main/java/sypztep/dominatus/common/system/skill/passives/agility/ShadowStepPassive.java
@@ -2,20 +2,24 @@ package sypztep.dominatus.common.system.skill.passives.agility;
 
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.attribute.EntityAttributeModifier;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.text.Text;
 import sypztep.dominatus.Dominatus;
+import sypztep.dominatus.common.component.living.LivingLevelComponent;
 import sypztep.dominatus.common.init.ModEntityAttributes;
+import sypztep.dominatus.common.init.ModEntityComponents;
+import sypztep.dominatus.common.system.skill.PassiveSkillManager;
 import sypztep.dominatus.common.system.skill.passives.BaseAttributePassive;
 
 /**
- * AGI 50 - Shadow Step: 15% chance to avoid damage completely
+ * AGI 50 - Shadow Step: foodstep is now gone
  */
 public class ShadowStepPassive extends BaseAttributePassive {
     public ShadowStepPassive() {
         super(
                 Dominatus.id("shadow_step"),
                 Text.literal("§8Shadow Step"),
-                Text.literal("15% chance to completely avoid incoming damage"),
+                Text.literal("No longer have footstep sound"),
                 "agility",
                 50,
                 4
@@ -24,13 +28,18 @@ public class ShadowStepPassive extends BaseAttributePassive {
 
     @Override
     protected void applyAttributeModifiers(LivingEntity entity) {
-        // This would need special handling in damage events
-        // For now, we'll add evasion as a representation
         applyAttributeModifier(entity, ModEntityAttributes.EVASION, Dominatus.id("passive_shadow_step"), 15.0, EntityAttributeModifier.Operation.ADD_VALUE);
     }
 
     @Override
     protected void removeAttributeModifiers(LivingEntity entity) {
         removeAttributeModifier(entity, ModEntityAttributes.EVASION, Dominatus.id("passive_shadow_step"));
+    }
+    public static boolean hasShadowStep(PlayerEntity entity) {
+        LivingLevelComponent levelComponent = ModEntityComponents.LIVINGLEVEL.get(entity);
+        PassiveSkillManager passiveManager = levelComponent.getPassiveSkillManager();
+
+        if (passiveManager == null) return false;
+        return passiveManager.isUnlocked(Dominatus.id("shadow_step"));
     }
 }
