e04b6ae3d0afd84d46669a6f7990cdcd5813e4b7
chain <chainot@hotmail.co.th>
2025-07-23 07:17:07 +0700

    Minecraft Particle Register

--- src/main/java/sypztep/dominatus/client/DominatusClient.java
+++ src/main/java/sypztep/dominatus/client/DominatusClient.java
@@ -4,10 +4,13 @@ import me.shedaniel.autoconfig.AutoConfig;
 import me.shedaniel.autoconfig.serializer.GsonConfigSerializer;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
+import net.fabricmc.fabric.api.client.particle.v1.ParticleFactoryRegistry;
 import sypztep.dominatus.ModConfig;
 import sypztep.dominatus.client.input.ModKeyBindings;
+import sypztep.dominatus.client.particle.ShockwaveParticle;
 import sypztep.dominatus.client.payload.*;
 import sypztep.dominatus.client.toast.ToastHudRenderer;
+import sypztep.dominatus.common.init.ModParticles;
 
 public class DominatusClient implements ClientModInitializer {
     public static ModConfig config = new ModConfig();
@@ -20,6 +23,10 @@ public class DominatusClient implements ClientModInitializer {
         ClientPlayNetworking.registerGlobalReceiver(AddTextParticlesPayloadS2C.ID, new AddTextParticlesPayloadS2C.Receiver());
         ClientPlayNetworking.registerGlobalReceiver(AddEmitterParticlePayloadS2C.ID, new AddEmitterParticlePayloadS2C.Receiver());
         ClientPlayNetworking.registerGlobalReceiver(SendToastPayloadS2C.ID, new SendToastPayloadS2C.Receiver());
+        ClientPlayNetworking.registerGlobalReceiver(AddAirhikeParticlesPayloadS2C.ID, new AddAirhikeParticlesPayloadS2C.Receiver());
+
+        ParticleFactoryRegistry registry = ParticleFactoryRegistry.getInstance();
+        registry.register(ModParticles.AIRHIKE, ShockwaveParticle.Factory::new);
 
         ModKeyBindings.register();
 
