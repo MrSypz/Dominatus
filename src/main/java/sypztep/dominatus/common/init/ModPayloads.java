983b9212813f14d93e3e8b7bb53c71807f7c8904
chain <chainot@hotmail.co.th>
2025-07-23 07:15:28 +0700

    more payload

--- src/main/java/sypztep/dominatus/common/init/ModPayloads.java
+++ src/main/java/sypztep/dominatus/common/init/ModPayloads.java
@@ -3,6 +3,7 @@ package sypztep.dominatus.common.init;
 import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
 import net.fabricmc.fabric.api.networking.v1.ServerPlayNetworking;
 import sypztep.dominatus.client.payload.*;
+import sypztep.dominatus.common.payload.AirHikePayloadC2S;
 import sypztep.dominatus.common.payload.IncreaseStatPayloadC2S;
 
 public final class ModPayloads {
@@ -13,12 +14,15 @@ public final class ModPayloads {
         PayloadTypeRegistry.playS2C().register(AddTextParticlesPayloadS2C.ID, AddTextParticlesPayloadS2C.CODEC); // Server to Client
         PayloadTypeRegistry.playS2C().register(AddEmitterParticlePayloadS2C.ID, AddEmitterParticlePayloadS2C.CODEC); // Server to Client
         PayloadTypeRegistry.playS2C().register(SendToastPayloadS2C.ID, SendToastPayloadS2C.CODEC);
+        PayloadTypeRegistry.playS2C().register(AddAirhikeParticlesPayloadS2C.ID, AddAirhikeParticlesPayloadS2C.CODEC);
 
         PayloadTypeRegistry.playC2S().register(IncreaseStatPayloadC2S.ID, IncreaseStatPayloadC2S.CODEC); // Client to Server
+        PayloadTypeRegistry.playC2S().register(AirHikePayloadC2S.ID, AirHikePayloadC2S.CODEC); // Client to Server
 
         registerPayloads();
     }
     private static void registerPayloads() {
         ServerPlayNetworking.registerGlobalReceiver(IncreaseStatPayloadC2S.ID, new IncreaseStatPayloadC2S.Receiver());
+        ServerPlayNetworking.registerGlobalReceiver(AirHikePayloadC2S.ID, new AirHikePayloadC2S.Receiver());
     }
 }
